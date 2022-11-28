35634d28ffffb563c696e6e39cdce69434f7b258
Batı Gökovalı <batigokovali@icloud.com>
2022-11-28 16:06:57 +0300

    Initial commit

---
+++ variable_exercises.js
@@ -0,0 +1,22 @@
+console.log("Welcome to the first JavaScript Homework!");
+
+// First, I'll declare the variables.
+
+let myName = "Batı";
+let mySurname = "Gökovalı";
+let mySpace = " ";
+let myFullName = myName + mySpace + mySurname;
+
+//Then, I'll print my name on the console.
+
+console.log("Hello, my name is:", myFullName);
+
+//Now, I'll declare the variables for my age and current year.
+
+myAge = 25;
+currentYear = 2022;
+birthYear = currentYear - myAge;
+
+//Then, I'll print my birth year to console.
+
+console.log("My birth year is:", birthYear);
